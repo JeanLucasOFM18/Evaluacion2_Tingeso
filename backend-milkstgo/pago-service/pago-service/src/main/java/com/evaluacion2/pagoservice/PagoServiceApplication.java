5184b48c8a253a593bcb14d637fdfc9922240225
Jean Lucas Rivera <jean.rivera@usach.cl>
2023-06-13 17:42:02 -0400

    Testeo

--- backend-milkstgo/pago-service/pago-service/src/main/java/com/evaluacion2/pagoservice/PagoServiceApplication.java
+++ backend-milkstgo/pago-service/pago-service/src/main/java/com/evaluacion2/pagoservice/PagoServiceApplication.java
@@ -2,10 +2,10 @@ package com.evaluacion2.pagoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class PagoServiceApplication {
 
 	public static void main(String[] args) {
