41bef566057608eed69e4a8eb50b257d09803d4c
Jean Lucas Rivera <jean.rivera@usach.cl>
2023-06-13 16:50:26 -0400

    Eureka y Gateway completados, falta testeo

--- backend-milkstgo/gateway-service/gateway-service/src/main/java/com/evaluacion2/gatewayservice/GatewayServiceApplication.java
+++ backend-milkstgo/gateway-service/gateway-service/src/main/java/com/evaluacion2/gatewayservice/GatewayServiceApplication.java
@@ -1,4 +1,4 @@
-package com.evaluacion2.pagoservice;
+package com.evaluacion2.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class PagoServiceApplication {
+public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PagoServiceApplication.class, args);
+		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
 }
--- backend-milkstgo/porcentaje-service/porcentaje-service/src/main/java/com/evaluacion2/porcentajeservice/PorcentajeServiceApplication.java
+++ backend-milkstgo/porcentaje-service/porcentaje-service/src/main/java/com/evaluacion2/porcentajeservice/PorcentajeServiceApplication.java
@@ -2,8 +2,10 @@ package com.evaluacion2.porcentajeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PorcentajeServiceApplication {
 
 	public static void main(String[] args) {
